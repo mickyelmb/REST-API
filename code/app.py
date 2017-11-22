abd196813d7884a06da4644405d9952019769b60
Michael <mickyel50@gmail.com>
2017-11-21 19:32:12 -0500

    Added PostgreSQL environment varialbe and pyscopg2

--- code/app.py
+++ code/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Api
@@ -7,8 +9,9 @@ from resources.store import Store, StoreList
 from  resources.user import UserRegister
 from security import authenticate, identity
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'mike'
 api = Api(app)
